46f55417f71560e3b7926dd6421ee23ec0c602b6
Reed Shea <reedshea@gmail.com>
2024-12-23 11:07:29 -0500

    Icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,32 @@
 <script>
+    
+  const icons = {
+    scroll: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 20c-3.2 0-4-2.667-4-4H3v2a2 2 0 0 0 2 2zm0 0a3 3 0 0 0 3-3v-7m0-6H7a2 2 0 0 0-2 2v10M18 4h1a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-1m0-6v6m-4-2H9m2 4H9"/></svg>',
+    crown: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M5 20v-2h14v2zm0-3.5L3.725 8.475q-.05 0-.113.013T3.5 8.5q-.625 0-1.062-.438T2 7t.438-1.062T3.5 5.5t1.063.438T5 7q0 .175-.038.325t-.087.275L8 9l3.125-4.275q-.275-.2-.45-.525t-.175-.7q0-.625.438-1.063T12 2t1.063.438T13.5 3.5q0 .375-.175.7t-.45.525L16 9l3.125-1.4q-.05-.125-.088-.275T19 7q0-.625.438-1.063T20.5 5.5t1.063.438T22 7t-.437 1.063T20.5 8.5q-.05 0-.112-.012t-.113-.013L19 16.5z"/></svg>',
+    calendar: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 14q-.425 0-.712-.288T11 13t.288-.712T12 12t.713.288T13 13t-.288.713T12 14m-4 0q-.425 0-.712-.288T7 13t.288-.712T8 12t.713.288T9 13t-.288.713T8 14m8 0q-.425 0-.712-.288T15 13t.288-.712T16 12t.713.288T17 13t-.288.713T16 14m-4 4q-.425 0-.712-.288T11 17t.288-.712T12 16t.713.288T13 17t-.288.713T12 18m-4 0q-.425 0-.712-.288T7 17t.288-.712T8 16t.713.288T9 17t-.288.713T8 18m8 0q-.425 0-.712-.288T15 17t.288-.712T16 16t.713.288T17 17t-.288.713T16 18M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.587 1.413T19 22zm0-2h14V10H5z"/></svg>',
+    users: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M230.4 219.19A8 8 0 0 1 224 232H32a8 8 0 0 1-6.4-12.8A67.9 67.9 0 0 1 53 197.51a40 40 0 1 1 53.93 0a67.4 67.4 0 0 1 21 14.29a67.4 67.4 0 0 1 21-14.29a40 40 0 1 1 53.93 0a67.85 67.85 0 0 1 27.54 21.68M27.2 126.4a8 8 0 0 0 11.2-1.6a52 52 0 0 1 83.2 0a8 8 0 0 0 12.8 0a52 52 0 0 1 83.2 0a8 8 0 0 0 12.8-9.61A67.85 67.85 0 0 0 203 93.51a40 40 0 1 0-53.93 0a67.4 67.4 0 0 0-21 14.29a67.4 67.4 0 0 0-21-14.29a40 40 0 1 0-53.93 0A67.9 67.9 0 0 0 25.6 115.2a8 8 0 0 0 1.6 11.2"/></svg>'
+  };
+
   const features = [
     {
       title: "Ancient wisdom, modern results",
       description: "Discover fitness routines that have stood the test of time",
-      icon: "scroll"
+      icon: icons.scroll
     },
     {
       title: "Governor's Circle Premium Access",
       description: "Join our elite training program for divine results",
-      icon: "crown"
+      icon: icons.crown
     },
     {
       title: "Group Classes",
       description: "Train with fellow disciples in our state-of-the-art facilities",
-      icon: "users"
+      icon: icons.users
     },
     {
       title: "6-Day Trial (plus a rest day)",
       description: "Experience a transformation of biblical proportions",
-      icon: "calendar"
+      icon: icons.calendar
     }
   ];
 
@@ -111,7 +119,7 @@
   .features {
     max-width: 72rem;
     margin: 0 auto;
-    padding: 3rem 1rem;
+    padding: 1rem 1rem;
   }
 
   .features-grid {
@@ -279,7 +287,7 @@
         <div class="feature-card">
           <div class="icon">
             <!-- You'll need to add your preferred icon library -->
-            <span>{feature.icon}</span>
+            <span>{@html feature.icon}</span>
           </div>
           <h3>{feature.title}</h3>
           <p>{feature.description}</p>
